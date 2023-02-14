ecb6c432815f23089426dcfa2168c89b4b96167a
Esha Kundu <esha.kundu@innoraft.com>
2023-02-14 12:40:31 +0530

    FT2023-55: Add new page forresult

---
+++ php1/result.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Preview</title>
+  <link rel="stylesheet" href="css/style1.css">
+</head>
+<body>
+  <?php
+  $full_name = $_SESSION["fullname"];
+  $file_store = $_SESSION['file'];
+  ?>
+
+  <div class="container">
+    <div class="result">
+      <h2>
+        <?php
+        //Viewing image file
+        echo "<img src='$file_store' style='width:150px;height:150px; border-radius:50%'><br>"; 
+        echo "Hello\n";
+        ?>
+      </h2>
+      <?php
+      echo $full_name;
+      ?>
+  </div>
+</body>
+</html>
